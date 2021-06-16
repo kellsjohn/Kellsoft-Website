4acf0776cfe8560243df8e410807ed01d3877059
Jobin John <jobinjohn664@gmail.com>
2021-06-16 13:54:04 +0530

    contact form completed

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ $(document).ready(function(){
         }
     })
 })
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+    url:"https://script.google.com/macros/s/AKfycbxdqcoLxkFB8nZMUnELHlK62cEEW5rC6ScalDdZC5qBI9ofjQW3khIe4IAZ7DNy-0Z-/exec",
+    data:$("#submit-form").serialize(),
+    method:"post",
+    success:function (response){
+        alert("Form submitted successfully")
+        window.location.reload()
+        //window.location.href="https://google.com"
+        },
+        error:function (err){
+        alert("Something Error")
+        }
+    })
+})
